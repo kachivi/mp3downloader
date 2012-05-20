d04292a7f71d32a65ff6d27edce34536d874a794
unknown <kachi@Kachi.(none)>
2012-05-20 15:54:50 +0530

    init

---
+++ src/WebPage.java
@@ -0,0 +1,9 @@
+import org.watij.webspec.dsl.WebSpec;
+
+public class WebPage {
+	public static void main(String[] args) throws Exception {
+		String baseUrl = "http://www.google.com";
+		WebSpec spec = new WebSpec().ie();
+		spec.open("http://www.google.com"); //opens google.com in Mozilla
+	}
+}
